8ac0f750a8dc6ecbfa07d60c28b2ecd85462ff32
Sidney Wells <sidneywells101@gmail.com>
2024-10-31 07:32:06 -0700

    Button component updates

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import "./button.css";
 import { Button as NextButton } from "@nextui-org/react";
-import { buttonStyles } from './button.css'
 
 interface ButtonProps {
   /** Is this the principal call to action on the page? */
   primary?: boolean;
   /** Color version of button */
-  color: string;
+  color: 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger';
   /** Radius of the button */
   radius?: "sm" | "md" | "lg" | "none" | "full";
-  /** What background color to use */
-  backgroundColor?: string;
-  /** Variant type */
-  variant?: "faded" | "bordered" | "light" | "flat" | "ghost" | "shadow";
   /** How large should the button be? */
   size?: "sm" | "md" | "lg";
-  /** Button contents */
-  label: string;
+  /** Variant type */
+  variant?: "faded" | "bordered" | "light" | "flat" | "ghost" | "shadow";
   /** Classname applies css to component */
   className: string;
   /** Text inside button */
@@ -28,26 +23,27 @@ interface ButtonProps {
 
 /** Primary UI component for user interaction */
 const Button = ({
-  primary = false,
+  primary,
   color,
-  size = "md",
+  size,
   radius,
   variant,
-  onClick,
+  className,
   children,
+  onClick,
   ...props
 }: ButtonProps) => {
-  // const mode = primary
-  //   ? "storybook-button--primary"
-  //   : "storybook-button--secondary";
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
   return (
     <NextButton
       color={color}
       size={size}
       radius={radius}
       variant={variant}
-      styles={buttonStyles}
       onClick={onClick}
+      className={['storybook-button'].join(' ')}
       {...props}
     >
       {children}
