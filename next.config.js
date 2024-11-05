479301fc855f52cbaa34d4137c59109235ee2efa
Sidney Wells <sidneywells101@gmail.com>
2024-11-05 12:02:04 -0800

    next config update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }, 
+  env: {
+    // use this as {process.env.customKey} 
+    customKey: 'fake value',
+  },
+}
