96b79a88b7b28fd7fd9d10edd7632204bc739320
Sidney Wells <sidneywells101@gmail.com>
2024-11-06 09:04:06 -0800

    Fix storybook build error about image

--- src/components/Image/Image.stories.ts
+++ src/components/Image/Image.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 // import { fn } from '@storybook/test';
-import Image from ".";
+import { Image } from "../utils/index.js";
 
 const meta = {
   title: "Example/Image",
